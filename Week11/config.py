9605789e639c149eb5589bdd5d7a31e8cece0938
Jessica Soler <solerj@wncc.edu>
2024-03-29 17:43:31 -0600

    filled in code
    need to change background to grey and add stripes

---
+++ Week11/config.py
@@ -0,0 +1,44 @@
+"""
+ Name: config.py
+ Author: Jessica Soler
+ Date: 3/28/24
+ Purpose: global variables and constants for the entire program
+"""
+import pygame
+import config
+from sys import exit
+
+# initialize constants for screen size
+WIDTH = 400
+HEIGHT = 600
+
+class CarCrash:
+    
+    def __init__(self):
+    
+        # initialize the pygame library
+        pygame.init()
+    
+        # create the game surface (window)
+        self.surface = pygame.display.set_mode(
+            (config.WIDTH, config.HEIGHT)
+        )
+        
+        # set window caption
+        pygame.display.set_caption("Car Crash")
+        
+        # load backgroundimage from file into an image variable
+        self.background = pygame.image.load(
+            "./assets/street.png").convert_alpha()
+        
+        # setup computer clock object to control the speed of the game
+        self.clock = pygame.time.Clock()
+        
+class Color:
+    def __init__(self):
+        pygame.init()
+        
+        BLACK = pygame.Color(0, 0, 0)
+        WHITE = pygame.Color (255, 255, 255)
+        GREY = pygame.Color(128, 128, 128)
+        RED = pygame.Color(255, 0, 0)
